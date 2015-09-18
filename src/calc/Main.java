dcbe39aca03e1c09e4f15125fc8f2133e28a4580
Alexandra <sakujo8744@mail.ru>
2015-09-18 21:47:37 +0300

    started

--- src/calc/Main.java
+++ src/calc/Main.java
@@ -1,13 +1,31 @@
 package calc;
 
+import java.io.File;
+
 /**
  * Created by alexandra on 9/18/15.
  */
 public class Main {
 
     public static void main(String[] args) {
+        String path = "resources";
+        File directory = new File(path);
+        if(!directory.exists()){
+            System.out.printf("Directory %s does not exists", path);
+            return;
+        }
+        if(!directory.isDirectory()){
+            System.out.printf("File %s is not a directory", path);
+            return;
+        }
+        File[] files = directory.listFiles();
+        for(File file : files){
 
+        }
     }
 
+    public static void work(File file){
+
+    }
 
 }
