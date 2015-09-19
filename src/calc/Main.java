a8e560557808f06e675c7621721161b5034b6300
Alexandra <sakujo8744@mail.ru>
2015-09-19 12:55:32 +0300

    save to file implemented

--- src/calc/Main.java
+++ src/calc/Main.java
@@ -1,6 +1,15 @@
 package calc;
 
-import java.io.File;
+import calc.operations.Multiplication;
+import calc.operations.Operation;
+import calc.operations.SqrSum;
+import calc.operations.Sum;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by alexandra on 9/18/15.
@@ -18,17 +27,50 @@ public class Main {
             System.out.printf("File %s is not a directory", path);
             return;
         }
+        Map<Integer,Operation> ops = new HashMap<>();
+        ops.put(1, new Sum());
+        ops.put(2, new Multiplication());
+        ops.put(3, new SqrSum());
+
+
         File[] files = directory.listFiles();
+        int result = 0;
         for(File file : files){
             if(file.getName().matches("in_\\d+")){
-                work(file);
+                result += work(file, ops);
             }
 
         }
+        System.out.println(result);
+
+        try(PrintWriter printWriter = new PrintWriter(new FileOutputStream("resources/out"))){
+            printWriter.print(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public static void work(File file){
+    public static int work(File file, Map<Integer,Operation> ops){
+        try(Scanner scanner = new Scanner(new FileInputStream(file))){
+            int action = scanner.nextInt();
+            System.out.println(action);
+            ArrayList<Integer> numbers = new ArrayList<>();
 
+            while(scanner.hasNext()){
+                numbers.add(scanner.nextInt());
+            }
+
+            int result = ops.get(action).perform(numbers);
+
+            System.out.println(numbers);
+            System.out.println(result);
+            return result;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
+
 
 }
